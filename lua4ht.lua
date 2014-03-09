9a63e35a50a347b9dce3b5c2daf2ab1300f66269
mr.mitch <mr.mitch@vfemail.net>
2014-03-10 00:09:34 +0100

    Exploring math

--- lua4ht.lua
+++ lua4ht.lua
@@ -9,18 +9,25 @@ local disc_id = 7
 local whatsit_id = 8
 local glue_id = 10
 local kern_id = 11
+local noad_id = node.id("noad")
+local sub_mlist_id = node.id("sub_mlist")
+local sub_box_id = node.id("sub_box")
+local frac_id = node.id("fraction")
+local radical_id = node.id("radical")
+local delim_id = node.id("delim")
+local fence_id = node.id("fence")
 local utfchar = unicode.utf8.char
 local types = node.types()
-
+local math_char_id = node.id("math_char") 
 local input =  nil --io.open(tex.jobname .. ".txt","w")
 local opened_files = {}
 local file_stack = {}
 
 function bit(p) 
-	return 2 ^ (p - 1) -- 1-based indexing 
+  return 2 ^ (p - 1) -- 1-based indexing 
 end -- Typical call: if hasbit(x, bit(3)) then ... i
 function hasbit(x, p) 
-	return x % (p + p) >= p 
+  return x % (p + p) >= p 
 end
 
 WriterClass = {}
@@ -122,12 +129,12 @@ process =  function(head)
       if n.subtype > 0 and n.components then
         chr = process(n.components)
       else
-				local s = n.subtype
-				if not hasbit(s,bit(1)) then
+        local s = n.subtype
+        if not hasbit(s,bit(1)) then
           chr = utfchar(n.char)
-				else
-					print("Bit 1 set",n.char,s)
-				end
+        else
+          print("Bit 1 set",n.char,s)
+        end
       end
       w:write(chr)
       table.insert(current, chr)
@@ -138,13 +145,60 @@ process =  function(head)
       table.insert(current, process(n.head))
     elseif id == vlist_id then 
       table.insert(current, process(n.head))
+    elseif id == sub_mlist_id then
+      w:all("sub_mlist[")
+      table.insert(current, process(n.head))
+      w:all "]"
+    elseif id == sub_box_id then
+      w:all("sub_box[")
+      table.insert(current, process(n.head))
+      w:all("]")
+    elseif id == noad_id then
+      w:all  ("\nnoad["..n.subtype.."\n")
+      table.insert(current, process(n.nucleus))
+      w:all "\n noad sup: "
+      process(n.sup)
+      process(n.sub)
+      w:all "]\n"
+    elseif  id == math_char_id then
+      w:all(utfchar(n.char)..":"..n.fam)
+    elseif id == frac_id then
+      w:all "\nfraction[\n"
+      process(n.left)
+      process(n.num)
+      process(n.denom)
+      process(n.right)
+      w:all "]\n"
     elseif id == whatsit_id and n.subtype == 3 then
       local k =  process_tex4ht(n.data) -- "<!-- " .. n.data .. " -->"
       --w:write(k)
       table.insert(current, k)
-		else
-			local subtype = n.subtype or ""
-			w:all("["..types[id]..":"..subtype .."]")
+    elseif id == radical_id then
+      w:all "radical[\n"
+      w:all "sub:" 
+      process(n.sub)
+      w:all " sup:"
+      process(n.sup)
+      w:all " nucleus:"
+      process(n.nucleus)
+      w:all " left:"
+      process(n.left)
+      w:all " degree:"
+      process(n.degree)
+      w:all "\n]\n"
+    elseif id == fence_id then
+      w:all ("fence["..n.subtype.."\n")
+      process(n.delim)
+      w:all "\n]\n"
+    elseif id==delim_id then
+      w:all "delim[\n"
+      local small_char = n.small_char
+      local large_char = n.large_char
+      w:all(small_char .. ":"..large_char)
+      w:all "\n]\n"
+    else
+      local subtype = n.subtype or ""
+      w:all("["..id..":"..types[id]..":"..subtype .."]")
     end
   end
   return table.concat(current)
