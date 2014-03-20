d245e72828cb7ddea2e9e668c03e64d8d77bc8c9
Michal Hoftich <michal.h21@gmail.com>
2014-03-20 15:57:18 +0100

    Create base class for different callbacks

---
+++ l4callback_class.lua
@@ -0,0 +1,28 @@
+-- inheritanced method from http://stackoverflow.com/a/6927969/2467963
+
+local writer = require "l4writer"
+local callback = {}
+
+local mt_callback = {
+  writer = writer(),
+}
+
+mt_callback.__index = {
+	new = function(self,t)
+		return setmetatable(t or {}, {__index=self})
+	end,
+	inherit=function (self,t,methods)
+		local mtnew={__index=setmetatable(methods,{__index=self})}
+		return setmetatable(t or {},mtnew)
+	end,
+	hej = function(self)
+		print(self.hello)
+	end,
+	hello = "world"
+	
+}
+
+setmetatable(callback, mt_callback)
+
+local k = callback:new()
+k:hej()
