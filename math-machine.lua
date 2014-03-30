ce66c551618b977fda10d8367f3edd1393012c2b
mr.mitch <mr.mitch@vfemail.net>
2014-03-30 23:21:18 +0200

    New math callback

---
+++ math-machine.lua
@@ -0,0 +1,43 @@
+local clb = require "node-machine"
+
+local mnode = clb:inherit({},{ill=function(self,s) self:all(s.."\n") end })
+
+local char = unicode.utf8.char
+
+mnode:default (16) (function(self)
+  local n = self.node
+  self:ill("math node "..n.subtype)
+  self:ill("ncleus")
+  self:run(n.nucleus)
+  self:ill("sub")
+  self:run(n.sub)
+  self:ill("sup")
+  self:run(n.sup)
+end)
+
+-- math char
+mnode:default(31) (function(self)
+  local n= self.node
+  local id = n.fam
+  local fnt = n.font
+  local families = {"math italic", "symbols", "extension", "it text", "slanted text", "bold text", "typewriter", [0]="roman"}
+  local name=families[id] or "Unknown family"
+  --local name = fnt.fullname or fnt.name
+  self:ill ("math.char: "..char(n.char).." : "..fnt)
+end)
+
+-- sub_box
+mnode:default(32) (function(self)
+  local n= self.node
+  self:ill("sub_box")
+  self:run(n.head)
+end)
+
+-- sub_mlist
+mnode:default (33) (function(self)
+  local n = self.node
+  self:ill("sub_mlist")
+  self:run(n.head)
+end)
+
+return mnode
